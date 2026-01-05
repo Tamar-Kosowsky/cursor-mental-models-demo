686f154b1fda55810146646aa3bfce4690b33ca8
tamar.kosovsky <tamar.kosovsky@entrata.com>
2026-01-05 10:21:52 +0200

    deleted project

---
+++ .cursor/tab.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def main():
+    print(add(1, 2))
+
+def division(a, b):
